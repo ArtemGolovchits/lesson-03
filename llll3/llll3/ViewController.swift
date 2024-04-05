844f90d0d3a3a2c508070b16ccb1cbbbb0d30e2b
Artem <artemgolovchits@icloud.com>
2024-04-05 20:57:10 +0300

    lesson 03 created

---
+++ llll3/llll3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  llll3
+//
+//  Created by Artem Golovchits on 5.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
